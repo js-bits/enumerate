be81e10b57e1b3837d26d051dd50b5da0f16e93d
ApoloGH <apolo-gh@users.noreply.github.com>
2021-04-18 19:22:48 -0400

    feat: add more converters

--- index.js
+++ index.js
@@ -1,10 +1,54 @@
-const convert = (acc, item) => {
+const converters = new Map();
+
+converters.set(String, (acc, item) => {
+  acc[item] = item;
+  return acc;
+});
+
+converters.set(Symbol, (acc, item) => {
   acc[item] = Symbol(item);
   return acc;
+});
+
+converters.set(Number, (acc, item) => {
+  acc[item] = Object.keys(acc).length;
+  return acc;
+});
+
+const convert = (list, type = Symbol) => {
+  if (typeof type !== 'function') {
+    throw new Error('Invalid Converter');
+  }
+
+  const converter = converters.get(type) || type;
+  const values = list.trim().split(/[\s\n,]+/);
+  const accumulator = {};
+  const result = values.reduce(converter, accumulator);
+
+  if (result !== accumulator) {
+    throw new Error('Invalid Converter');
+  }
+  return result;
 };
 
-export default ([str]) =>
-  str
-    .trim()
-    .split(/[\s\n,]+/)
-    .reduce(convert, {});
+const enumerate = (...args) => {
+  if (args.length > 2) {
+    throw new Error('Invalid Arguments');
+  }
+
+  if (!Array.isArray(args[0])) {
+    if (args.length !== 1) {
+      throw new Error('Invalid Arguments');
+    }
+
+    const [type] = args;
+    return (...rest) => enumerate(...rest, type);
+  }
+
+  const [list] = args[0];
+  const type = args[1];
+
+  return convert(list, type);
+};
+
+export default enumerate;
