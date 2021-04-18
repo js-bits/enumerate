687d9ee1acdbaf0d18b41393547f1bbf9aa21e9d
ApoloGH <apolo-gh@users.noreply.github.com>
2021-04-18 13:10:14 -0400

    test: add more unit-tests

--- index.test.js
+++ index.test.js
@@ -7,15 +7,27 @@ describe('enumerate', () => {
     expect(Episode).toHaveProperty('EMPIRE');
     expect(Episode).toHaveProperty('JEDI');
   });
-  describe('return object', () => {
-    test('should have unique property values', () => {
+
+  describe('return object properties', () => {
+    test('should have unique values', () => {
       const { NEW_HOPE, EMPIRE, JEDI } = Episode;
       expect(NEW_HOPE).not.toBe(EMPIRE);
       expect(NEW_HOPE).not.toBe(JEDI);
       expect(EMPIRE).not.toBe(JEDI);
+
       expect(NEW_HOPE).not.toEqual(EMPIRE);
       expect(NEW_HOPE).not.toEqual(JEDI);
       expect(EMPIRE).not.toEqual(JEDI);
+    });
+
+    test('should have values unique globally', () => {
+      const Unit1 = enumerate`FOOT, METER`;
+      const Unit2 = enumerate`FOOT, METER`;
+      expect(Unit1.FOOT).not.toBe(Unit2.FOOT);
+      expect(Unit1.METER).not.toBe(Unit2.METER);
+
+      expect(Unit1.FOOT).not.toEqual(Unit2.FOOT);
+      expect(Unit1.METER).not.toEqual(Unit2.METER);
     });
   });
 });
